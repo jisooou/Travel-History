5a1dfe9b3924e31424ec7dd6f4fa134c8220bb9d
jisooou <jisoou4@gmail.com>
2026-04-26 22:54:19 +0900

    feat: User Entity의 UUID Column Converter

--- src/main/java/com/project/travel/converter/UUIDConverter.java
+++ src/main/java/com/project/travel/converter/UUIDConverter.java
@@ -1,4 +1,30 @@
 package com.project.travel.converter;
 
-public class UUIDConverter {
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+import java.nio.ByteBuffer;
+import java.util.UUID;
+
+@Converter(autoApply = false)
+public class UUIDConverter implements AttributeConverter<UUID, byte[]> {
+    @Override
+    public byte[] convertToDatabaseColumn(UUID uuid) {
+        if (uuid == null) {
+            return null;
+        }
+        ByteBuffer bb = ByteBuffer.wrap(new byte[16]);
+        bb.putLong(uuid.getMostSignificantBits());
+        bb.putLong(uuid.getLeastSignificantBits());
+        return bb.array();
+    }
+
+    @Override
+    public UUID convertToEntityAttribute(byte[] bytes) {
+        if (bytes == null) {
+            return null;
+        }
+        ByteBuffer bb = ByteBuffer.wrap(bytes);
+        return new UUID(bb.getLong(), bb.getLong());
+    }
 }
