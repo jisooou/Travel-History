65a45fcbb90c9cfa3273d3460bfcecf04c8b40d1
jisooou <jisoou4@gmail.com>
2026-04-28 22:06:29 +0900

    feat: UserName ErrorCode 추가

--- src/main/java/com/project/travel/global/exception/ErrorCode.java
+++ src/main/java/com/project/travel/global/exception/ErrorCode.java
@@ -9,7 +9,8 @@ public enum ErrorCode {
     //    USER
     DUPLICATE_EMAIL(400, "U001", "이미 존재하는 이메일입니다."),
     PASSWORD_MISMATCH(400, "U002", "비밀번호가 일치하지 않습니다."),
-    USER_NOT_FOUND(404, "U003", "사용자를 찾을 수 없습니다.");
+    USER_NOT_FOUND(404, "U003", "사용자를 찾을 수 없습니다."),
+    USER_ALREADY_EXIST(400, "U004", "이미 존재하는 사용자입니다.");
 
     private final int status;
     private final String code;
