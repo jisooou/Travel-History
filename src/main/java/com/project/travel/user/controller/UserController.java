69c70b8f647a0f4f0961414ba23235ece0332ab1
jisooou <jisoou4@gmail.com>
2026-04-29 21:17:48 +0900

    feat: Email SignUp API

--- src/main/java/com/project/travel/user/controller/UserController.java
+++ src/main/java/com/project/travel/user/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.project.travel.user.controller;
 
 import com.project.travel.global.response.ApiResponse;
+import com.project.travel.user.dto.request.EmailSendRequestDto;
+import com.project.travel.user.dto.request.EmailVerifyRequestDto;
 import com.project.travel.user.dto.request.UserSignUpRequestDto;
 import com.project.travel.user.dto.response.UserSignUpResponseDto;
+import com.project.travel.user.service.EmailService;
 import com.project.travel.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/users")
 public class UserController {
     private final UserService userService;
+    private final EmailService emailService;
+
+    @PostMapping("/signup/email/send")
+    public ApiResponse<Void> sendEmail(@RequestBody EmailSendRequestDto requestDto) {
+        emailService.sendEmailCode(requestDto.getEmail());
+        return ApiResponse.success(null);
+    }
+
+    @PostMapping("/signup/email/verify")
+    public ApiResponse<Void> verifyEmail(@RequestBody EmailVerifyRequestDto reqeustDto) {
+        emailService.verifyEmail(reqeustDto.getEmail(), reqeustDto.getCode());
+        return ApiResponse.success(null);
+    }
 
     @PostMapping("/signup")
     public ApiResponse<UserSignUpResponseDto> signUp(@RequestBody UserSignUpRequestDto requestDto) {
