29f23167b1452913e6adcd44836dea90204d5c4f
jisooou <jisoou4@gmail.com>
2026-04-29 21:16:46 +0900

    feat: Email ErrorCode

--- src/main/java/com/project/travel/global/exception/ErrorCode.java
+++ src/main/java/com/project/travel/global/exception/ErrorCode.java
@@ -10,7 +10,11 @@ public enum ErrorCode {
     DUPLICATE_EMAIL(400, "U001", "이미 존재하는 이메일입니다."),
     PASSWORD_MISMATCH(400, "U002", "비밀번호가 일치하지 않습니다."),
     USER_NOT_FOUND(404, "U003", "사용자를 찾을 수 없습니다."),
-    USER_ALREADY_EXIST(400, "U004", "이미 존재하는 사용자입니다.");
+    USER_ALREADY_EXIST(400, "U004", "이미 존재하는 사용자입니다."),
+    //    EMAIL
+    EMAIL_CODE_EXPIRED(400, "E001", "이메일 인증 코드가 만료되었습니다."),
+    EMAIL_CODE_MISMATCH(400, "E002", "이메일 인증 코드가 올바르지 않습니다."),
+    NOT_VERIFIED_EMAIL(400, "E003", "잘못된 이메일입니다.");
 
     private final int status;
     private final String code;
