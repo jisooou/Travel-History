3b1b4ab728d7c90ae8e61d74dd2611233e4d44d7
jisooou <jisoou4@gmail.com>
2026-05-08 22:27:51 +0900

    feat: add authorizeHttpRequest API

--- src/main/java/com/project/travel/config/SecurityConfig.java
+++ src/main/java/com/project/travel/config/SecurityConfig.java
@@ -1,13 +1,23 @@
 package com.project.travel.config;
 
+import com.project.travel.auth.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -15,16 +25,25 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
+        return http
                 .csrf(csrf -> csrf.disable())
+                .formLogin(form -> form.disable())
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                "/api/v1/users/signup",
-                                "/api/v1/users/signup/email/send",
-                                "/api/v1/users/signup/email/verify"
-                        ).permitAll()
-                        .anyRequest().authenticated()
-                );
-        return http.build();
+                                .requestMatchers(
+//                                로그인 없이 접근 가능한 API
+                                        "/api/v1/users/signup",
+                                        "/api/v1/users/signup/email/send",
+                                        "/api/v1/users/signup/email/verify",
+                                        "/api/v1/auth/login",
+                                        "/api/v1/auth/reissue"
+                                ).permitAll()
+                                .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 }
