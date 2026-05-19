b75e951d7c9d2129d7c510a0427fd87a553579d8
jisooou <jisoou4@gmail.com>
2026-05-19 22:21:47 +0900

    feat: create Todo Service

--- src/main/java/com/project/travel/todo/service/TodoService.java
+++ src/main/java/com/project/travel/todo/service/TodoService.java
@@ -1,4 +1,28 @@
 package com.project.travel.todo.service;
 
+import com.project.travel.todo.dto.request.TodoCreateRequestDto;
+import com.project.travel.todo.dto.request.TodoStatusUpdateRequestDto;
+import com.project.travel.todo.dto.response.TodoResponseDto;
+import com.project.travel.todo.dto.response.TodoStatusResponseDto;
+import jakarta.validation.Valid;
+
+import java.util.List;
+
 public class TodoService {
+    public TodoResponseDto createTodo(Integer userNo, Integer dayNo, @Valid TodoCreateRequestDto createRequestDto) {
+    }
+
+    public List<TodoResponseDto> getTodoOfDay(Integer userNo, Integer dayNo) {
+        return null;
+    }
+
+    public TodoResponseDto updateTodoOfDay(Integer userNo, Integer todoNo, @Valid TodoCreateRequestDto createRequestDto) {
+    }
+
+    public TodoStatusResponseDto updateTodoStatus(Integer userNo, Integer todoNo, @Valid TodoStatusUpdateRequestDto requestDto) {
+    }
+
+    public void deleteTodo(Integer userNo, Integer todoNo) {
+
+    }
 }
