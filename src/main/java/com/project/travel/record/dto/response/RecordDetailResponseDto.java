61993a497065462c7ac8d9594b2bdf498b5eae2f
jisooou <jisoou4@gmail.com>
2026-05-19 22:20:03 +0900

    feat: add Record 상세페이지 Dto

--- src/main/java/com/project/travel/record/dto/response/RecordDetailResponseDto.java
+++ src/main/java/com/project/travel/record/dto/response/RecordDetailResponseDto.java
@@ -1,5 +1,6 @@
 package com.project.travel.record.dto.response;
 
+import com.project.travel.record.entity.Record;
 import com.project.travel.schedule.dto.response.ScheduleResponseDto;
 import com.project.travel.todo.dto.response.TodoResponseDto;
 import lombok.Builder;
@@ -10,8 +11,23 @@ import java.util.List;
 @Getter
 @Builder
 public class RecordDetailResponseDto {
+    //    1개의 Record 안에 여러 Days 가능
     private RecordResponseDto record;
     private List<RecordDayResponseDto> days;
     private List<ScheduleResponseDto> schedules;
     private List<TodoResponseDto> todos;
+
+    public static RecordDetailResponseDto of(
+            Record record,
+            List<RecordDayResponseDto> days,
+            List<ScheduleResponseDto> schedules,
+            List<TodoResponseDto> todos
+    ) {
+        return RecordDetailResponseDto.builder()
+                .record(RecordResponseDto.from(record))
+                .days(days)
+                .schedules(schedules)
+                .todos(todos)
+                .build();
+    }
 }
