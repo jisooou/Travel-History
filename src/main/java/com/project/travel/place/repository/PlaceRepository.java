ecfab78eef8618d9f114d3e64347579a3464cc93
jisooou <jisoou4@gmail.com>
2026-05-24 23:45:36 +0900

    feat: add Optional method

--- src/main/java/com/project/travel/place/repository/PlaceRepository.java
+++ src/main/java/com/project/travel/place/repository/PlaceRepository.java
@@ -4,7 +4,10 @@ import com.project.travel.place.entity.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PlaceRepository extends JpaRepository<Place, Integer> {
     List<Place> findByRecord_RecordNo(Integer recordNo);
+
+    Optional<Place> findByRecord_RecordNoAndMapSourceAndMapPlaceId(Integer recordNo, String mapSource, String mapPlaceId);
 }
