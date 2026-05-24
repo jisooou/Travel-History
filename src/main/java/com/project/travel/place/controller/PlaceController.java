0fa29ac6aef1a0cb572a78468947241459e6fe02
jisooou <jisoou4@gmail.com>
2026-05-24 23:04:40 +0900

    feat: Place initial API

--- src/main/java/com/project/travel/place/controller/PlaceController.java
+++ src/main/java/com/project/travel/place/controller/PlaceController.java
@@ -1,4 +1,11 @@
 package com.project.travel.place.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/place")
 public class PlaceController {
 }
