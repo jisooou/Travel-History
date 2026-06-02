3d2d485dc87e0a1788b545202385a146719a5ef9
jisooou <jisoou4@gmail.com>
2026-06-03 01:51:57 +0900

    feat: add ErroCode_Collab, Invite

--- src/main/java/com/project/travel/global/exception/ErrorCode.java
+++ src/main/java/com/project/travel/global/exception/ErrorCode.java
@@ -10,6 +10,7 @@ public enum ErrorCode {
     DUPLICATE_EMAIL(400, "U001", "이미 존재하는 이메일입니다."),
     PASSWORD_MISMATCH(400, "U002", "비밀번호가 일치하지 않습니다."),
     USER_ALREADY_EXIST(400, "U003", "이미 존재하는 사용자입니다."),
+    EMAIL_NOT_FOUND(404, "U004", "가입된 이메일이 아닙니다."),
     //    EMAIL
     EMAIL_CODE_EXPIRED(400, "E001", "이메일 인증 코드가 만료되었습니다."),
     EMAIL_CODE_MISMATCH(400, "E002", "이메일 인증 코드가 올바르지 않습니다."),
@@ -33,7 +34,23 @@ public enum ErrorCode {
     SCHEDULE_ACCESS_DENIED(400, "S002", "해당 일정에 접근할 수 없습니다."),
     //    TODO
     TODO_NOT_FOUND(404, "T001", "해당 투두를 찾을 수 없습니다."),
-    TODO_ACCESS_DENIED(400, "T002", "해당 투두에 접근할 수 없습니다.");
+    TODO_ACCESS_DENIED(400, "T002", "해당 투두에 접근할 수 없습니다."),
+    //COLLAB
+    COLLAB_AUTHORITY_OWNER(404, "C001", "OWNER 권한이 필요합니다."),
+    COLLAB_AUTHORITY_EDITOR(404, "C002", "수정 권한이 없습니다."),
+    COLLAB_AUTHORITY_VIEWER(404, "C003", "조회 권한이 없습니다."),
+    //GUEST_CODE
+    GUEST_CODE_GENERATED_FAIL(400, "GC001", "참여코드 발급에 실패했습니다."),
+    GUEST_CODE_INVALID_CODE(400, "GC002", "유효하지 않은 참여코드입니다."),
+    GUEST_CODE_INVALID_RECORD(400, "GC003", "유효하지 않은 (참여)기록입니다."),
+    GUEST_CODE_EXPIRED_CODE(400, "GC004", "만료된 참여코드입니다."),
+    GUEST_DUPLICATED_NAME(400, "GC005", "이미 사용중인 비회원 이름입니다."),
+    //INVITE
+    INVITE_INVALID_AUTHORITY_OWNER(400, "I001", "Owner를 초대할 수는 없습니다."),
+    INVITE_STATUS_PENDING(400, "I002", "이미 대기중 상태인 초대가 있습니다."),
+    INVITE_STATUS_NOT_FOUND_ACCEPT_AND_REJECT(400, "I003", "수락/거절 가능한 초대가 존재하지 않습니다."),
+    INVITE_STATUS_DIFFERENT_EMAIL(400, "I004", "초대받은 이메일과 로그인한 이메일이 일치하지 않습니다."),
+    INVITE_COLLAB_ALREADY_EXiST(400, "I005", "이미 참여중인 기록입니다.");
 
     private final int status;
     private final String code;
