c6c41f7ba0f93f1dc1a0b9d6e89cee653e109e8c
jisooou <jisoou4@gmail.com>
2026-06-17 14:28:25 +0900

    feat: add update sortOrder query

--- src/main/java/com/project/travel/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/project/travel/schedule/repository/ScheduleRepository.java
@@ -1,7 +1,14 @@
 package com.project.travel.schedule.repository;
 
+import com.project.travel.record.entity.TimeSlot;
 import com.project.travel.schedule.entity.SchedulePlace;
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -9,4 +16,21 @@ public interface ScheduleRepository extends JpaRepository<SchedulePlace, Integer
     List<SchedulePlace> findByDay_DayNo(Integer dayNo);
 
     List<SchedulePlace> findByDay_Record_RecordNo(Integer recordNo);
+
+    List<SchedulePlace> findByDay_DayNoOrderByTimeSlotSortOrderAsc(Integer dayNo);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints({
+            @QueryHint(name = "jakarta.persistence.lock.timeout", value = "3000")
+    })
+    @Query("""
+            select sp
+            from SchedulePlace sp
+            where sp.day.dayNo = :dayNo
+            and sp.timeSlot = :timeSlot
+            """)
+    List<SchedulePlace> findByDayNoAndTimeSlotForUpdate(
+            @Param("dayNo") Integer dayNo,
+            @Param("timeSlot") TimeSlot timeSlot
+    );
 }
