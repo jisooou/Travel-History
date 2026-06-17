5a7b8da5d33dc042818acbf5536c62192bded7e8
jisooou <jisoou4@gmail.com>
2026-06-17 14:27:20 +0900

    feat: add Reorder RequestDto

--- src/main/java/com/project/travel/schedule/dto/request/ScheduleOrderRequestDto.java
+++ src/main/java/com/project/travel/schedule/dto/request/ScheduleOrderRequestDto.java
@@ -1,4 +1,13 @@
 package com.project.travel.schedule.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
 public class ScheduleOrderRequestDto {
+    @NotNull
+    private Integer scheduleNo;
+
+    @NotNull
+    private Integer sortOrder;
 }
--- src/main/java/com/project/travel/schedule/dto/request/ScheduleReorderRequestDto.java
+++ src/main/java/com/project/travel/schedule/dto/request/ScheduleReorderRequestDto.java
@@ -1,4 +1,19 @@
 package com.project.travel.schedule.dto.request;
 
+import com.project.travel.record.entity.TimeSlot;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
 public class ScheduleReorderRequestDto {
+    @NotNull
+    private TimeSlot timeSlot;
+
+    @NotEmpty
+    @Valid
+    private List<ScheduleOrderRequestDto> schedules;
 }
