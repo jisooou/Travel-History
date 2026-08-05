9bd703c479b0fdfd64363a0ec1114064bc46db1c
jisooou <jisoou4@gmail.com>
2026-08-05 16:54:21 +0900

    feat: todo writer N+1 entitygraph 사용하여 해결

--- src/main/java/com/project/travel/todo/repository/TodoRepository.java
+++ src/main/java/com/project/travel/todo/repository/TodoRepository.java
@@ -1,6 +1,7 @@
 package com.project.travel.todo.repository;
 
 import com.project.travel.todo.entity.Todo;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,5 +9,6 @@ import java.util.List;
 public interface TodoRepository extends JpaRepository<Todo, Integer> {
     List<Todo> findByDay_DayNoOrderByCreatedAtAsc(Integer dayNo);
 
+    @EntityGraph(attributePaths = "writer")
     List<Todo> findByDay_Record_RecordNoAndDay_Record_IsDeletedFalseOrderByCreatedAtAsc(Integer recordNo);
 }
