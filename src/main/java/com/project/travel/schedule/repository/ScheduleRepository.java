334d8c0ffe176bd28e8178f36d93a6f24cbc9f21
jisooou <jisoou4@gmail.com>
2026-08-05 16:53:56 +0900

    feat: place N+1 entitygraph 사용하여 해결

--- src/main/java/com/project/travel/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/project/travel/schedule/repository/ScheduleRepository.java
@@ -4,10 +4,7 @@ import com.project.travel.record.entity.TimeSlot;
 import com.project.travel.schedule.entity.SchedulePlace;
 import jakarta.persistence.LockModeType;
 import jakarta.persistence.QueryHint;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.QueryHints;
+import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
@@ -15,6 +12,7 @@ import java.util.List;
 public interface ScheduleRepository extends JpaRepository<SchedulePlace, Integer> {
     List<SchedulePlace> findByDay_DayNo(Integer dayNo);
 
+    @EntityGraph(attributePaths = "place")
     List<SchedulePlace> findByDay_Record_RecordNoAndDay_Record_IsDeletedFalse(Integer recordNo);
 
     List<SchedulePlace> findByDay_DayNoOrderByTimeSlotAscSortOrderAsc(Integer dayNo);
