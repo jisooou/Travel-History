4ac9fd74ce8d95b7e22abc137f64ae0bdbc2d23f
jisooou <jisoou4@gmail.com>
2026-08-10 02:06:46 +0900

    feat: user의 record 조회할 때 owner, editor 같이 조회하기 위해 query 작성

--- src/main/java/com/project/travel/record/repository/RecordRepository.java
+++ src/main/java/com/project/travel/record/repository/RecordRepository.java
@@ -1,13 +1,33 @@
 package com.project.travel.record.repository;
 
+import com.project.travel.collab.entity.RoleCode;
 import com.project.travel.record.entity.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface RecordRepository extends JpaRepository<Record, Integer> {
-    List<Record> findByOwner_UserNoAndIsDeletedFalse(Integer userNo);
+    boolean existsByRecordNoAndOwner_UserNoAndIsDeletedFalse(Integer recordNo, Integer userNo);
 
     Optional<Record> findByRecordNoAndIsDeletedFalse(Integer recordNo);
+
+    @Query("""
+            select distinct r 
+            from Record r
+            left join Collab c
+            on c.record = r
+            where r.isDeleted = false
+            and 
+            (
+            r.owner.userNo = :userNo
+            or (c.user.userNo = :userNo and c.roleCode = :roleCode)
+            )
+            """)
+    List<Record> findMyRecords(
+            @Param("userNo") Integer userNo,
+            @Param("roleCode") RoleCode roleCode
+    );
 }
